73465f2742e27e8e48cf2d1547daf67e2b8ba998
mathesond2 <mathesond2@gmail.com>
2023-07-06 11:46:24 -0400

    styles

--- app/(editor)/collect/page.tsx
+++ app/(editor)/collect/page.tsx
@@ -4,10 +4,10 @@ import Editor from '@/app/components/Editor';
 export default function Page() {
   return (
     <div className="h-screen grid grid-cols-24 grid-rows-6">
-      <div className="z-10 col-start-1 col-span-12 bg-indigo-500 h-screen rounded-tr-[60px] rounded-br-[50px]">
+      <div className="col-start-1 col-span-12 bg-black h-screen rounded-tr-[60px] rounded-br-[50px] z-10">
         <Dashboard />
       </div>
-      <div className="bg-blue-500 col-start-12 col-span-full row-start-1 row-end-7">
+      <div className="col-start-12 col-span-full row-start-1 row-end-7">
         <Editor />
       </div>
     </div>
