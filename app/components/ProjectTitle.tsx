4acbb979ff7148f1e477b471b6a919355f1720f4
mathesond2 <mathesond2@gmail.com>
2023-07-06 13:37:50 -0400

    style

--- app/components/ProjectTitle.tsx
+++ app/components/ProjectTitle.tsx
@@ -16,7 +16,7 @@ export default function ProjectTitle() {
       type="text"
       value={title}
       onChange={handleChange}
-      className="bg-transparent border-none font-bold text-4xl focus-visible:outline-0 mb-8"
+      className="bg-transparent border-none font-bold text-4xl focus-visible:outline-0 mb-8 w-11/12"
     />
   );
 }
