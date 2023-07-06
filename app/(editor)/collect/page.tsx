828e105247de908b222ebc1528c433d226ea47af
mathesond2 <mathesond2@gmail.com>
2023-07-06 11:35:43 -0400

    add layout styles

--- app/(editor)/collect/page.tsx
+++ app/(editor)/collect/page.tsx
@@ -3,11 +3,11 @@ import Editor from '@/app/components/Editor';
 
 export default function Page() {
   return (
-    <div className="columns-2">
-      <div className="w-full">
+    <div className="h-screen grid grid-cols-24 grid-rows-6">
+      <div className="z-10 col-start-1 col-span-12 bg-indigo-500 h-screen rounded-tr-[60px] rounded-br-[50px]">
         <Dashboard />
       </div>
-      <div className="w-full">
+      <div className="bg-blue-500 col-start-12 col-span-full row-start-1 row-end-7">
         <Editor />
       </div>
     </div>
--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
-    <div className="bg-indigo-500 h-screen rounded-tr-[60px] rounded-br-[40px]">
+    <>
       <h1>Dashboard</h1>
-    </div>
+    </>
   );
 }
