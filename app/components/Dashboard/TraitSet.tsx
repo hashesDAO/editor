db8e664214678450c13e55ab2a9ee70598623d5e
mathesond2 <mathesond2@gmail.com>
2023-07-07 10:58:50 -0400

    add letter-spacing, icon

--- app/components/Dashboard/TraitSet.tsx
+++ app/components/Dashboard/TraitSet.tsx
@@ -1,4 +1,5 @@
 import { TraitType } from '@/app/util';
+import { IoInformationCircleOutline } from 'react-icons/io5';
 
 type Props = React.PropsWithChildren<{
   traitType: TraitType;
@@ -7,7 +8,10 @@ type Props = React.PropsWithChildren<{
 export default function TraitSet({ traitType, children }: Props) {
   return (
     <section className="w-1/2 mb-8">
-      <p className="text-xs opacity-40 font-bold">{traitType}</p>
+      <p className="text-xs opacity-40 font-medium tracking-wide">
+        {traitType}
+        <IoInformationCircleOutline className="text-sm inline-flex ml-2 cursor-pointer" />
+      </p>
       {children}
     </section>
   );
