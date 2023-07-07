da2662b82ca31d54e8f549f3fc4ce7e36aff4e69
mathesond2 <mathesond2@gmail.com>
2023-07-07 12:34:29 -0400

    add DashboardFooter

---
+++ app/components/Dashboard/DashboardFooter.tsx
@@ -0,0 +1,12 @@
+export default function DashboardFooter() {
+  return (
+    <footer
+      className="fixed bg-black rounded-br-[50px] left-0 bottom-0 w-overflow-y-auto p-4 border-t border-[#272727] flex justify-end items-center px-8 p-8 w-1/2"
+      style={{
+        boxShadow: '0px -46px 64px 0px #131313',
+      }}
+    >
+      <button>MINT</button>
+    </footer>
+  );
+}
--- app/components/Dashboard/index.tsx
+++ app/components/Dashboard/index.tsx
@@ -3,22 +3,26 @@ import { ARTISTIC, AUGMENT, GEOMETRY, PATTERNS } from '@/app/util/constants';
 import ControlMenu from './ControlMenu';
 import ProjectTitle from './ProjectTitle';
 import TraitSet from './TraitSet';
+import DashboardFooter from './DashboardFooter';
 
 const traitTypes: TraitType[] = [ARTISTIC, AUGMENT, GEOMETRY, PATTERNS];
 
 export default function Dashboard() {
   return (
-    <section className="p-4">
-      <ProjectTitle />
-      <p>Select element here</p>
-      <ControlMenu />
-      <div className="flex flex-wrap">
-        {traitTypes.map((traitType) => (
-          <TraitSet key={traitType} traitType={traitType}>
-            <p>traitType</p>
-          </TraitSet>
-        ))}
+    <section className="relative h-full overflow-y-auto">
+      <div className="p-4">
+        <ProjectTitle />
+        <p>Select element here</p>
+        <ControlMenu />
+        <div className="flex flex-wrap">
+          {traitTypes.map((traitType) => (
+            <TraitSet key={traitType} traitType={traitType}>
+              <p>traitType</p>
+            </TraitSet>
+          ))}
+        </div>
       </div>
+      <DashboardFooter />
     </section>
   );
 }
