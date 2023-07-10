8dce5b060f65edfa3755af50f8c88b5a4f2d7f8e
mathesond2 <mathesond2@gmail.com>
2023-07-10 10:03:41 -0400

    create wrapper comp HashSelect to move forthcoming conditional logic down

---
+++ app/components/Dashboard/HashSelect.tsx
@@ -0,0 +1,35 @@
+import Select from '../Select';
+import GenerateButton from './GenerateButton';
+import HashPill from './HashPill';
+
+const selectOptions = [
+  {
+    label: '0xhey',
+    value: '0xhey',
+  },
+  {
+    label: '0xgoodmorning0xgoodmorning0xgoodmorning0xgoodmorning0xgoodmorning0xgoodmorning',
+    value: '0xgoodmorning0xgoodmorning0xgoodmorning0xgoodmorning0xgoodmorning0xgoodmorning',
+  },
+  {
+    label: '0xhowdy',
+    value: '0xhowdy',
+  },
+];
+
+export default function HashSelect() {
+  return (
+    <div className="flex mb-8">
+      <div className="w-full">
+        <HashPill />
+      </div>
+      <div className="w-4/6">
+        <Select options={selectOptions} />
+      </div>
+      <div className="w-2/6 flex flex-row items-center">
+        <p className="px-4">OR</p>
+        <GenerateButton />
+      </div>
+    </div>
+  );
+}
