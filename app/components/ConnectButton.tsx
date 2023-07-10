7da1b8a143ca3cff2bb7898f24aaef7692697bd8
mathesond2 <mathesond2@gmail.com>
2023-07-10 15:19:16 -0400

    remove top nav, add custom connect button

--- app/components/ConnectButton.tsx
+++ app/components/ConnectButton.tsx
@@ -1,5 +1,36 @@
-import { ConnectButton as RainbowKitButton } from '@rainbow-me/rainbowkit';
+'use client';
+
+import { ConnectButton as RainbowConnectButton } from '@rainbow-me/rainbowkit';
+import Button from './Button';
+
+const CustomButton = ({
+  clickHandler,
+  text,
+  buttonColor,
+}: {
+  clickHandler: () => void;
+  text: string;
+  buttonColor?: string;
+}) => <Button text={text} buttonColor={buttonColor || 'bg-baseBlack'} onClick={clickHandler} />;
 
 export default function ConnectButton() {
-  return <RainbowKitButton />;
+  return (
+    <RainbowConnectButton.Custom>
+      {({ account, chain, openAccountModal, openChainModal, openConnectModal, mounted }) => (
+        <div>
+          {(() => {
+            if (!mounted || !account || !chain) {
+              return <CustomButton clickHandler={openConnectModal} text="CONNECT WALLET" buttonColor="bg-primaryRed" />;
+            }
+
+            if (chain.unsupported) {
+              return <CustomButton clickHandler={openChainModal} text="Wrong network" />;
+            }
+
+            return <CustomButton clickHandler={openAccountModal} text={`connected as ${account.displayName}`} />;
+          })()}
+        </div>
+      )}
+    </RainbowConnectButton.Custom>
+  );
 }
--- app/components/Dashboard/DashboardFooter.tsx
+++ app/components/Dashboard/DashboardFooter.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import ConnectButton from '../ConnectButton';
 import Mint from './buttons/Mint';
+import { useAccount } from 'wagmi';
 
 export default function DashboardFooter() {
+  const { isConnected } = useAccount();
   return (
     <footer
       className="fixed bg-black rounded-br-[50px] left-0 bottom-0 w-overflow-y-auto p-4 border-t border-[#272727] flex justify-end items-center px-8 p-8 w-1/2"
@@ -8,7 +13,14 @@ export default function DashboardFooter() {
         boxShadow: '0px -46px 64px -12px #131313',
       }}
     >
-      <Mint />
+      {isConnected ? (
+        <>
+          <ConnectButton />
+          <Mint />
+        </>
+      ) : (
+        <ConnectButton />
+      )}
     </footer>
   );
 }
