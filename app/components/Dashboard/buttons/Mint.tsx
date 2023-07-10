cfa878928c12e955a09825ac9c5f9ffc180e9725
mathesond2 <mathesond2@gmail.com>
2023-07-10 15:24:06 -0400

    move connection logic from footer to mint button

--- app/components/Dashboard/buttons/Mint.tsx
+++ app/components/Dashboard/buttons/Mint.tsx
@@ -1,8 +1,15 @@
 'use client';
 
+import { useAccount } from 'wagmi';
 import Button from '../../Button';
 
 export default function Mint() {
+  const { isConnected } = useAccount();
+
+  if (!isConnected) {
+    return null;
+  }
+
   return (
     <Button
       text={'MINT'}
