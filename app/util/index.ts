620f95161d98762de5e760900443ba27d82e749e
mathesond2 <mathesond2@gmail.com>
2023-07-11 15:51:49 -0400

    refactor prior ethers work to viem

--- app/util/index.ts
+++ app/util/index.ts
@@ -1,42 +1,27 @@
+import { createPublicClient, getContract, http } from 'viem';
+import { goerli, mainnet } from 'viem/chains';
 import { ARTISTIC, AUGMENT, GEOMETRY, PATTERNS } from './constants';
-import HASHES_ABI from './Hashes.json';
-import type { Network } from './types';
+import { hashesContract } from './hashesContract';
 
 export type TraitType = typeof ARTISTIC | typeof AUGMENT | typeof GEOMETRY | typeof PATTERNS;
 
-export const NETWORKS: Record<string, Network> = {
-  homestead: {
-    title: 'Ethereum',
-    rpcUrl: 'https://mainnet.infura.io/v3/%INFURA_API_KEY%',
-    explorerUrl: 'https://etherscan.io',
-    explorerApiUrl: 'https://api.etherscan.io/api',
-    chainId: '1',
-  },
-  mainnet: {
-    title: 'Ethereum',
-    rpcUrl: 'https://mainnet.infura.io/v3/%INFURA_API_KEY%',
-    explorerUrl: 'https://etherscan.io',
-    explorerApiUrl: 'https://api.etherscan.io/api',
-    chainId: '1',
-  },
-  goerli: {
-    title: 'Goerli',
-    rpcUrl: 'https://goerli.infura.io/v3/%INFURA_API_KEY%',
-    explorerUrl: 'https://goerli.etherscan.io',
-    explorerApiUrl: 'https://api-goerli.etherscan.io/api',
-    chainId: '5',
-  },
-};
-
-export const HASHES_ADDRESS: { [key: string]: string } = {
+export const HASHES_ADDRESS: { [key: string]: `0x${string}` } = {
   homestead: '0xD07e72b00431af84AD438CA995Fd9a7F0207542d',
   mainnet: '0xD07e72b00431af84AD438CA995Fd9a7F0207542d',
   goerli: '0x2Fe6A4F23ac78c137Ce7D2aD9108a607b624AF5C',
 };
 
-export function getHashesContract(chain: string): ethers.Contract {
-  const rpcUrl = NETWORKS[chain].rpcUrl.replace('%INFURA_API_KEY%', process.env['NEXT_PUBLIC_INFURA_API_KEY']!);
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-  const newContract = new ethers.Contract(HASHES_ADDRESS[chain], HASHES_ABI.abi, provider);
-  return newContract;
+export function getHashesContract(chain: string) {
+  const client = createPublicClient({
+    chain: chain === 'goerli' ? goerli : mainnet,
+    transport: http(),
+  });
+
+  const contract = getContract({
+    ...hashesContract,
+    address: HASHES_ADDRESS[chain],
+    publicClient: client,
+  });
+
+  return contract;
 }
--- app/util/types.ts
+++ app/util/types.ts
@@ -1,7 +1,3 @@
-export type Network = {
-  title: string;
-  rpcUrl: string;
-  explorerUrl: string;
-  explorerApiUrl: string;
-  chainId: string;
-};
+import { ARTISTIC, AUGMENT, GEOMETRY, PATTERNS } from './constants';
+
+export type TraitType = typeof ARTISTIC | typeof AUGMENT | typeof GEOMETRY | typeof PATTERNS;
