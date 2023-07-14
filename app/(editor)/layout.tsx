787fca51de741821adee0672d25beed570a282c6
mathesond2 <mathesond2@gmail.com>
2023-07-14 13:32:53 -0400

    create /contexts, remove unnecessary div

--- app/(editor)/layout.tsx
+++ app/(editor)/layout.tsx
@@ -1,17 +1,10 @@
 import '@rainbow-me/rainbowkit/styles.css';
-import RainbowkitProviders from '../RainbowkitProviders';
-// import ConnectButton from '../components/ConnectButton';
-// import Link from 'next/link';
+import RainbowkitProviders from '../contexts/RainbowkitProviders';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <RainbowkitProviders>
-      {/* <nav className="bg-black flex items-center justify-between py-4 px-4 mb-4 border-b-2 border-white">
-        <Link href={'/'}>Home</Link>
-        <ConnectButton />
-      </nav> */}
-      <main>
-        <div className="h-screen grid grid-cols-24 grid-rows-6">{children}</div>
-      </main>
+      <main className="h-screen grid grid-cols-24 grid-rows-6">{children}</main>
     </RainbowkitProviders>
   );
 }
