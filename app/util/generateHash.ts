e9acca8963ef63a481d1967a97d8e03b6cc0be73
mathesond2 <mathesond2@gmail.com>
2023-07-17 09:37:44 -0400

    successfully generated test

--- app/util/generateHash.ts
+++ app/util/generateHash.ts
@@ -1,5 +1,11 @@
 import { keccak256, encodePacked, Address } from 'viem';
 
 export function generateHash(input: string, address: Address) {
-  return keccak256(encodePacked(['uint256', 'address', 'string'], [BigInt(461), address, input]));
+  // return keccak256(encodePacked(['uint256', 'address', 'string'], [BigInt(461), address, input]));
+  return keccak256(
+    encodePacked(
+      ['uint256', 'address', 'string'],
+      [BigInt(1063), '0x9fdc28740f4ccd64e51451480d0a2a4df5b98f90', 'smoking'],
+    ),
+  );
 }
