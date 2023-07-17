747f8860f1baab9adf0a9eedda544999abfac6af
mathesond2 <mathesond2@gmail.com>
2023-07-17 10:44:51 -0400

    add state for 'edit mode' and values in HashSelect

--- app/components/Dashboard/HashSelect.tsx
+++ app/components/Dashboard/HashSelect.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import useHashesData from '@/app/hooks/useHashesData';
+import { useState } from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
 import type { HashesData } from '../../util/types';
+import Button from '../common/Button';
+import CircleButton from '../common/CircleButton';
 import Select from '../common/Select';
 import HashPill from './HashPill';
 import Generate from './buttons/Generate';
@@ -18,8 +22,22 @@ function Section({ children }: { children: React.ReactNode }) {
 }
 
 export default function HashSelect() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newHashValue, setNewHashValue] = useState('');
   const { hashData, isError, isLoading } = useHashesData();
   const { hashes } = hashData || {};
+
+  function handleCreateButtonClick() {
+    setIsEditing(true);
+  }
+
+  function handleBackButtonClick() {
+    setIsEditing(false);
+  }
+
+  function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setNewHashValue(e.target.value);
+  }
 
   if (isError) {
     return (
@@ -42,11 +60,32 @@ export default function HashSelect() {
       {hashes && hashes.length > 0 ? (
         <Section>
           <div className="w-4/6">
-            <Select options={createHashSelectOptions(hashes)} />
+            {isEditing ? (
+              <div className="flex">
+                <CircleButton onClick={handleBackButtonClick}>
+                  <FaArrowLeft />
+                </CircleButton>
+                <input
+                  type="text"
+                  className="w-full py-4 px-5 bg-traitGray rounded-full"
+                  placeholder="Enter a phrase"
+                  value={newHashValue}
+                  onChange={handleOnChange}
+                />
+              </div>
+            ) : (
+              <Select options={createHashSelectOptions(hashes)} />
+            )}
           </div>
           <div className="w-2/6 flex flex-row items-center">
-            <p className="px-4">OR</p>
-            <Generate />
+            {isEditing ? (
+              <Generate onClick={() => {}} value={newHashValue} />
+            ) : (
+              <>
+                <p className="px-4">OR</p>
+                <Button text="CREATE NEW" onClick={handleCreateButtonClick} />
+              </>
+            )}
           </div>
         </Section>
       ) : (
