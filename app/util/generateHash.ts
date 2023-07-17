68f60d57ba9c9fea957f9b5f78d18096c91b1cbe
mathesond2 <mathesond2@gmail.com>
2023-07-17 09:07:06 -0400

    add viem type

--- app/util/generateHash.ts
+++ app/util/generateHash.ts
@@ -1,4 +1,4 @@
-import { keccak256, toHex, encodePacked, Address } from 'viem';
+import { keccak256, encodePacked, Address } from 'viem';
 
 export function generateHash(input: string, address: Address) {
   return keccak256(encodePacked(['uint256', 'address', 'string'], [BigInt(461), address, input]));
--- app/util/types.ts
+++ app/util/types.ts
@@ -1,3 +1,4 @@
+import { Address } from 'viem';
 import { ARTISTIC, AUGMENT, GEOMETRY, PATTERNS } from './constants';
 
 export type TraitType = typeof ARTISTIC | typeof AUGMENT | typeof GEOMETRY | typeof PATTERNS;
@@ -5,7 +6,7 @@ export type TraitType = typeof ARTISTIC | typeof AUGMENT | typeof GEOMETRY | typ
 export type ChainNames = 'homestead' | 'mainnet' | 'goerli';
 
 export type HashesData = {
-  hash_value: `0x${string}`;
+  hash_value: Address;
   type: 'DAO' | 'Standard';
   token_id: number;
 };
