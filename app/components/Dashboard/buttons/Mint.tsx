dd0bfc9b6017b4e14f83e951b107bdb34e487e55
mathesond2 <mathesond2@gmail.com>
2023-07-18 10:30:58 -0400

    component setup for calling mint()

--- app/components/Dashboard/buttons/Mint.tsx
+++ app/components/Dashboard/buttons/Mint.tsx
@@ -3,22 +3,30 @@
 import { useHashContext } from '@/app/contexts/HashContext';
 import { useAccount } from 'wagmi';
 import Button from '../../common/Button';
+import useHashesData from '@/app/hooks/useHashesData';
+
+function mint() {}
 
 export default function Mint() {
   const { isConnected } = useAccount();
   const selectedHash = useHashContext();
+  const { hashData } = useHashesData();
+  const { hashes } = hashData || {};
+  const parsedHashes = hashes?.map(({ hash_value }) => hash_value);
 
   if (!isConnected) {
     return null;
   }
 
-  return (
-    <Button
-      text={'UPDATE HASH'}
-      buttonColor={'bg-primaryRed'}
-      onClick={() => {
-        console.log(`mint with ${selectedHash}!`);
-      }}
-    />
-  );
+  function handleClick() {
+    console.log(`mint with ${selectedHash}!`);
+
+    if (!parsedHashes || !parsedHashes.includes(selectedHash)) {
+      // mint();
+    } else {
+      console.log(`hash found ${selectedHash}`);
+    }
+  }
+
+  return <Button text={'UPDATE HASH'} buttonColor={'bg-primaryRed'} onClick={handleClick} />;
 }
