ef6571884d3b1d0c87981b7b7542a80cc7290e98
mathesond2 <mathesond2@gmail.com>
2023-08-01 20:17:54 -0400

    move draggable trait functionality to its own comp

--- app/components/Dashboard/TraitList.tsx
+++ app/components/Dashboard/TraitList.tsx
@@ -1,101 +1,21 @@
 'use client';
 
-import { useContainerDimensions } from '@/app/hooks/useContainerDimensions';
-import { ParsedTrait, TraitObject } from '@/app/util/types';
-import { useRef, useState } from 'react';
-import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautiful-dnd';
-import DragTrait from './DragTrait';
+import { ParsedTrait } from '@/app/util/types';
+import DraggableTraitList from './DraggableTraitList';
 import Toggle from './Toggle';
 import Trait from './Trait';
 import TraitSet from './TraitSet';
-import { Drag } from './buttons/Drag';
 
 function isDragTrait(type: string) {
   return type === 'draw' || type === 'repeat';
 }
 
-function DraggableTraitList({ type, traits }: { type: string; traits: TraitObject[] }) {
-  const componentRef = useRef<HTMLDivElement>(null);
-  const { width: dynamicWidth } = useContainerDimensions(componentRef);
-
-  return (
-    <div ref={componentRef}>
-      <Droppable droppableId={type}>
-        {(provided) => (
-          <ul ref={provided.innerRef} {...provided.droppableProps}>
-            {traits.map(({ id, name, content }, index: number) => (
-              <Draggable key={id} draggableId={id} index={index}>
-                {(provided) => (
-                  <div
-                    {...provided.draggableProps}
-                    style={{
-                      ...provided.draggableProps.style,
-                      width: dynamicWidth,
-                    }}
-                  >
-                    <DragTrait
-                      key={id}
-                      name={name}
-                      value={{ id, content }}
-                      dragIcon={<Drag ref={provided.innerRef} {...provided.dragHandleProps} />}
-                    />
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
-    </div>
-  );
-}
-
-function reorderList(list: TraitObject[], startIndex: number, endIndex: number) {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-  return result;
-}
-
 export default function TraitList({ traits }: { traits: ParsedTrait[] }) {
   const draggableTraits = traits.filter(({ type }) => isDragTrait(type));
   const nonDraggableTraits = traits.filter(({ type }) => !isDragTrait(type));
-  const [dragTraits, setDragTraits] = useState(() => draggableTraits);
-
-  function handleDragEnd({ source, destination }: DropResult) {
-    // dropped outside the list
-    if (!destination) {
-      return;
-    }
-
-    const traitType = destination.droppableId;
-    const traitTypeToUpdate = dragTraits.find(({ type }) => type === traitType)?.traits || [];
-    const reorderedTraits = reorderList(traitTypeToUpdate, source.index, destination.index);
-
-    const newDragTraits = dragTraits.map((trait) => {
-      if (trait.type === traitType) {
-        return {
-          ...trait,
-          traits: reorderedTraits,
-        };
-      }
-      return trait;
-    });
-
-    setDragTraits(newDragTraits);
-  }
-
   return (
     <>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        {dragTraits.map(({ description, type, traits }) => (
-          <TraitSet key={type} title={type.toUpperCase()} info={description}>
-            <DraggableTraitList type={type} traits={traits} />
-          </TraitSet>
-        ))}
-      </DragDropContext>
-
+      <DraggableTraitList traits={draggableTraits} />
       {nonDraggableTraits.map(({ description, type, traits }) => (
         <TraitSet key={type} title={type.toUpperCase()} info={description}>
           <ul>
