1eef24a51d28a81d7070b372c2fa20bab5ba1e6f
mathesond2 <mathesond2@gmail.com>
2023-08-22 13:16:05 -0400

    wip

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -9,87 +9,66 @@ import type { Trait } from '../reducers/traitsReducer';
 import * as attributeLibrary from '../util/attributeLibrary';
 import { useTraitsContext } from '../contexts/TraitsContext';
 
+const p5 = require('p5'); // Import the p5.js module
+
 const htmlBoilerplate = `
 <html>
   <head>
     <meta charset="utf-8" />
+    <style>
+    .canvas-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    #myCanvas {
+      /* Add any additional styling here */
+    }
+
+    </style>
   </head>
   <body>
+    <div className="canvas-container">
+      <canvas id="myCanvas defaultCanvas0" class="p5Canvas" width="700" height="410"></canvas>
+    </div>
+    <h1>hello world</h1>
     <script src="sketch.js"></script>
   </body>
 </html>
 `;
 
 function createP5Drawing(hash: Address | string, traits: Trait[]) {
-  console.log('is initial?', traits.length === 0 || hash === INITIAL_SELECTED_HASH);
-  if (traits.length === 0 || hash === INITIAL_SELECTED_HASH) {
-    return `
-    const s = ( p5 ) => {
+  // if (traits.length === 0 || hash === INITIAL_SELECTED_HASH) {
+  //   return null;
+  // }
 
-      let x = 100;
-      let y = 100;
+  const sketch = (p) => {
+    let x = 100;
+    let y = 100;
 
-      p5.setup = function() {
-        p5.createCanvas(700, 410);
-        p5.background(255, 255);
-        p5.noLoop();
-      };
-
-      p5.draw = function() {
-        p5.background(255, 255);
-        p5.textSize(32);
-        p5.text('Select a hash and traits to get started.', 10, 30);
-      };
+    p.setup = function () {
+      p.createCanvas(700, 410);
+      p.background(255);
+      p.noLoop();
     };
 
-    let myp5 = new p5(s);
-  `;
-  } else {
-    const dude = (p5Name: any) =>
-      traits.reduce(
-        (prev, curr) => {
-          const traitWrapper = new Function('p5', 'lib', 'hash', curr.content);
-          const trait = traitWrapper(p5Name, attributeLibrary, hash);
-          console.log('firing', typeof p5Name);
-          return trait(prev);
-        },
-        () => {
-          console.log('emptyFn');
-        },
-      );
+    p.draw = function () {
+      //draw a circle in the middle of the canvas
+      p.ellipse(p.width / 2, p.height / 2, 50, 50);
+    };
+  };
 
-    console.log('dude', dude.toString());
-
-    return `
-      const s = ( p5 ) => {
-
-        let x = 100;
-        let y = 100;
-
-        p5.setup = function() {
-          p5.createCanvas(700, 410);
-          p5.background(255, 255);
-          p5.noLoop();
-        };
-
-        p5.draw = function() {
-          return ${dude('p5')}
-        };
-      };
-
-      let myp5 = new p5(s);
-      `;
-  }
+  const res = new p5(sketch);
+  console.log('res', res);
+  return res;
 }
 
 export default function Editor() {
   const { selectedHash } = useHashContext();
   const selectedTraits = useTraitsContext();
   const parsedHash = selectedHash === INITIAL_SELECTED_HASH ? '"0xhello"' : selectedHash;
-
-  const demo = createP5Drawing(parsedHash, selectedTraits);
-  console.log(demo);
-
   const files = useMemo(
     () => ({
       'sketch.js': {
@@ -98,23 +77,32 @@ export default function Editor() {
       },
       'index.html': {
         code: htmlBoilerplate,
-        hidden: true,
+        hidden: false,
       },
     }),
-    [parsedHash],
+    [parsedHash, selectedTraits],
   );
 
   return (
     <SandpackProvider
       theme="dark"
       template="static"
-      options={{
-        externalResources: ['https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.min.js'],
-      }}
+      options={
+        {
+          // externalResources: ['https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.min.js'],
+        }
+      }
       files={files}
       className="w-full"
     >
-      <SandpackPreview showOpenInCodeSandbox={false} showRefreshButton={false} className="h-screen" />
+      <SandpackPreview
+        showOpenInCodeSandbox={false}
+        showRefreshButton={false}
+        className="h-screen"
+        ref={(preview) => {
+          console.log('preview', preview);
+        }}
+      />
     </SandpackProvider>
   );
 }
