eb705d49ad19a56a26e91a584b903d78406f8680
mathesond2 <mathesond2@gmail.com>
2023-08-22 13:48:58 -0400

    use prior implementation

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -1,108 +1,60 @@
 'use client';
 
-import { SandpackPreview, SandpackProvider } from '@codesandbox/sandpack-react';
-import { useMemo } from 'react';
-import { useHashContext } from '../contexts/HashContext';
-import { INITIAL_SELECTED_HASH } from '../util/constants';
+import { useEffect, useRef } from 'react';
 import { Address } from 'viem';
+import { useHashContext } from '../contexts/HashContext';
+import { useTraitsContext } from '../contexts/TraitsContext';
 import type { Trait } from '../reducers/traitsReducer';
 import * as attributeLibrary from '../util/attributeLibrary';
-import { useTraitsContext } from '../contexts/TraitsContext';
 
-const p5 = require('p5'); // Import the p5.js module
+let p5: any;
+let p5Instance: any;
 
-const htmlBoilerplate = `
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <style>
-    .canvas-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-    }
+const renderP5 = (hash: Address | string, traits: Trait[]) => (p5: any) => {
+  const renderTraitStack = traits.reduce(
+    (prev, curr) => {
+      const traitWrapper = new Function('p5', 'lib', 'hash', curr.content);
+      const trait = traitWrapper(p5, attributeLibrary, hash);
+      return trait(prev);
+    },
+    () => {
+      console.log('emptyFn');
+    },
+  );
 
-    #myCanvas {
-      /* Add any additional styling here */
-    }
-
-    </style>
-  </head>
-  <body>
-    <div className="canvas-container">
-      <canvas id="myCanvas defaultCanvas0" class="p5Canvas" width="700" height="410"></canvas>
-    </div>
-    <h1>hello world</h1>
-    <script src="sketch.js"></script>
-  </body>
-</html>
-`;
-
-function createP5Drawing(hash: Address | string, traits: Trait[]) {
-  // if (traits.length === 0 || hash === INITIAL_SELECTED_HASH) {
-  //   return null;
-  // }
-
-  const sketch = (p) => {
-    let x = 100;
-    let y = 100;
-
-    p.setup = function () {
-      p.createCanvas(700, 410);
-      p.background(255);
-      p.noLoop();
-    };
-
-    p.draw = function () {
-      //draw a circle in the middle of the canvas
-      p.ellipse(p.width / 2, p.height / 2, 50, 50);
-    };
+  p5.setup = () => {
+    const container = document.querySelector('#canvas-container')!;
+    p5.createCanvas(container.clientHeight, container.clientHeight);
+    p5.background(255, 255);
+    p5.noLoop();
   };
 
-  const res = new p5(sketch);
-  console.log('res', res);
-  return res;
-}
+  p5.draw = () => {
+    renderTraitStack();
+  };
+};
 
 export default function Editor() {
+  const p5Ref = useRef(document.createElement('div'));
   const { selectedHash } = useHashContext();
   const selectedTraits = useTraitsContext();
-  const parsedHash = selectedHash === INITIAL_SELECTED_HASH ? '"0xhello"' : selectedHash;
-  const files = useMemo(
-    () => ({
-      'sketch.js': {
-        code: createP5Drawing(parsedHash, selectedTraits),
-        active: true,
-      },
-      'index.html': {
-        code: htmlBoilerplate,
-        hidden: false,
-      },
-    }),
-    [parsedHash, selectedTraits],
-  );
 
-  return (
-    <SandpackProvider
-      theme="dark"
-      template="static"
-      options={
-        {
-          // externalResources: ['https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.min.js'],
-        }
-      }
-      files={files}
-      className="w-full"
-    >
-      <SandpackPreview
-        showOpenInCodeSandbox={false}
-        showRefreshButton={false}
-        className="h-screen"
-        ref={(preview) => {
-          console.log('preview', preview);
-        }}
-      />
-    </SandpackProvider>
-  );
+  useEffect(() => {
+    p5 = require('p5');
+  }, []);
+
+  useEffect(() => {
+    if (!p5 || !selectedHash) {
+      return;
+    }
+    if (p5Instance) {
+      p5Instance.remove();
+    }
+    if (p5Ref.current.firstChild) {
+      p5Ref.current.removeChild(p5Ref.current.firstChild);
+    }
+    p5Instance = new p5(renderP5(selectedHash, selectedTraits), p5Ref.current);
+  }, [selectedHash, selectedTraits]);
+
+  return <div ref={p5Ref} className="h-screen" id="canvas-container"></div>;
 }
