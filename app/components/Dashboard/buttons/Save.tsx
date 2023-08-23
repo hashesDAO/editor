6415f31710b6ae605a5de96183aeec7c6ff5c84c
mathesond2 <mathesond2@gmail.com>
2023-08-23 16:45:14 -0400

    move useSave to its own file (4thalove)

--- app/components/Dashboard/buttons/Save.tsx
+++ app/components/Dashboard/buttons/Save.tsx
@@ -1,57 +1,10 @@
 'use client';
 
-import { useHashContext } from '@/app/contexts/HashContext';
-import { useProjectTitleContext } from '@/app/contexts/ProjectTitleContext';
-import { useTraitsContext } from '@/app/contexts/TraitsContext';
 import useCopyToClipboard from '@/app/hooks/useCopyToClipboard';
-import { Trait } from '@/app/reducers/traitsReducer';
-import { INITIAL_SELECTED_HASH, LOADING } from '@/app/util/constants';
-import { useReducer } from 'react';
+import useSave from '@/app/hooks/useSave';
 import { FaSave } from 'react-icons/fa';
 import { Tooltip } from 'react-tooltip';
-import { fetchReducer, initialFetchReducerState } from '../../../reducers/fetchReducer';
 import CircleButton from '../../common/CircleButton';
-
-function useSave() {
-  const [saveData, dispatchSaveData] = useReducer(fetchReducer, initialFetchReducerState);
-  const title = useProjectTitleContext();
-  const selectedTraits = useTraitsContext();
-  const { selectedHash } = useHashContext();
-  const isDisabled =
-    saveData.loading || title.length === 0 || selectedTraits.length === 0 || selectedHash === INITIAL_SELECTED_HASH;
-
-  async function handleSave() {
-    dispatchSaveData({ type: LOADING });
-    await fetch('/api/traits/save', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title,
-        traitIds: selectedTraits.map((trait: Trait) => trait.id),
-      }),
-    })
-      .then(async (res) => {
-        const payloadData = await res.json();
-        console.log('payloadData', payloadData);
-
-        dispatchSaveData({
-          type: 'SUCCESS',
-          payload: payloadData.slug,
-        });
-      })
-      .catch((err) => {
-        console.error(`error saving traits: ${err}`);
-        dispatchSaveData({
-          type: 'ERROR',
-          payload: err,
-        });
-      });
-  }
-
-  return { handleSave, isDisabled, saveData };
-}
 
 const errMessage = 'Error occurred while saving your project. Please try again.';
 const tooltip = {
