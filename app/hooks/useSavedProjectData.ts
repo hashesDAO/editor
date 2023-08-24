a4aa61c66c6925fd6a900baf87567f8a9cfa746a
mathesond2 <mathesond2@gmail.com>
2023-08-24 10:53:53 -0400

    buildfix

--- app/hooks/useSavedProjectData.ts
+++ app/hooks/useSavedProjectData.ts
@@ -6,13 +6,6 @@ import useSWR from 'swr';
 export default function useSavedProjectData() {
   const pathname = usePathname();
   const id = pathname.split('/').pop(); //TODO: rethink this, it's brittle
-  if (id === 'new') {
-    return {
-      data: null,
-      isLoading: false,
-      isError: null,
-    };
-  }
   // @ts-ignore
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(`/api/traits/${id}`, fetcher);
