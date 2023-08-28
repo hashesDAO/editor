ffde2084c62cd481f6dedf1b04dc3c457faedba2
mathesond2 <mathesond2@gmail.com>
2023-08-28 15:35:33 -0400

    consolidate consts

--- app/components/Dashboard/buttons/Mint.tsx
+++ app/components/Dashboard/buttons/Mint.tsx
@@ -4,7 +4,7 @@ import { useHashContext } from '@/app/contexts/HashContext';
 import useHashesData from '@/app/hooks/useHashesData';
 import useMintNewHash from '@/app/hooks/useMintNewHash';
 import useUpdate from '@/app/hooks/useUpdate';
-import { INITIAL_SELECTED_HASH, VERIFY_MESSAGE } from '@/app/util/constants';
+import { INITIAL_SELECTED_HASH, LOADING_TEXT, VERIFY_MESSAGE } from '@/app/util/constants';
 import { Tooltip } from 'react-tooltip';
 import { useAccount, useSignMessage } from 'wagmi';
 import Button from '../../common/Button';
@@ -12,9 +12,6 @@ import Button from '../../common/Button';
 type ButtonProps = {
   isLoadingHashesData: boolean;
 };
-
-const tooltipId = 'primary-action';
-const loadingText = '...LOADING';
 
 function noHashSelected(hash: string) {
   return hash === INITIAL_SELECTED_HASH;
@@ -24,10 +21,11 @@ function MintButton({ isLoadingHashesData }: ButtonProps) {
   const { selectedHash } = useHashContext();
   const { handleMint } = useMintNewHash();
   const isDisabled = isLoadingHashesData || noHashSelected(selectedHash);
+  const tooltipId = 'mint-tooltip';
   return (
     <>
       <Button
-        text={isLoadingHashesData ? loadingText : 'MINT HASH'}
+        text={isLoadingHashesData ? LOADING_TEXT : 'MINT HASH'}
         buttonColor={'bg-primaryRed'}
         onClick={handleMint}
         disabled={isDisabled}
@@ -56,6 +54,7 @@ function UpdateButton({ isLoadingHashesData }: ButtonProps) {
 
   const noHashSelected = selectedHash === INITIAL_SELECTED_HASH;
   const isDisabled = isDisabledViaSave || isLoadingHashesData || noHashSelected || isSignedLoading;
+  const tooltipId = 'update-tooltip';
 
   function handleClick() {
     signMessage();
@@ -67,7 +66,7 @@ function UpdateButton({ isLoadingHashesData }: ButtonProps) {
   return (
     <>
       <Button
-        text={isLoadingHashesData ? loadingText : 'UPDATE HASH'}
+        text={isLoadingHashesData ? LOADING_TEXT : 'UPDATE HASH'}
         buttonColor={'bg-primaryRed'}
         onClick={handleClick}
         disabled={isDisabled}
