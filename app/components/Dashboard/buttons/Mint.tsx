4dc8587e86f25d6a2c1eff25b9fad6760589fa74
mathesond2 <mathesond2@gmail.com>
2023-08-28 13:32:24 -0400

    add button tooltip

--- app/components/Dashboard/buttons/Mint.tsx
+++ app/components/Dashboard/buttons/Mint.tsx
@@ -5,12 +5,19 @@ import useMintNewHash from '@/app/hooks/useMintNewHash';
 import useUpdate from '@/app/hooks/useUpdate';
 import { useAccount } from 'wagmi';
 import Button from '../../common/Button';
+import { Tooltip } from 'react-tooltip';
+import { useHashContext } from '@/app/contexts/HashContext';
+import { INITIAL_SELECTED_HASH } from '@/app/util/constants';
 
+const tooltipId = 'primary-action';
+const loadingText = '...LOADING';
 export default function Mint() {
   const { isConnected } = useAccount();
   const { handleMint } = useMintNewHash();
+  const { selectedHash } = useHashContext();
   const { hashData, isError, isLoading: isLoadingHashesData } = useHashesData();
   const { handleSave, isDisabled: isDisabledViaSave } = useUpdate();
+  const noHashSelected = selectedHash === INITIAL_SELECTED_HASH;
 
   if (!isConnected) {
     return null;
@@ -18,15 +25,29 @@ export default function Mint() {
 
   const button = hashData?.hashes.length
     ? {
-        text: isLoadingHashesData ? '..LOADING' : 'UPDATE HASH',
+        text: isLoadingHashesData ? loadingText : 'UPDATE HASH',
         fn: handleSave,
         isDisabled: isDisabledViaSave || isLoadingHashesData,
+        tooltipText: 'Create a design and select a Hash to update.',
       }
     : {
-        text: isLoadingHashesData ? '...LOADING' : 'MINT HASH',
+        text: isLoadingHashesData ? loadingText : 'MINT HASH',
         fn: handleMint,
-        isDisabled: isLoadingHashesData,
+        isDisabled: isLoadingHashesData || noHashSelected,
+        tooltipText: 'Generate a new Hash before minting.',
       };
 
-  return <Button text={button.text} buttonColor={'bg-primaryRed'} onClick={button.fn} disabled={button.isDisabled} />;
+  return (
+    <>
+      <Button
+        text={button.text}
+        buttonColor={'bg-primaryRed'}
+        onClick={button.fn}
+        disabled={button.isDisabled}
+        data-tooltip-id={tooltipId}
+        data-tooltip-content={button.tooltipText}
+      />
+      {button.isDisabled && <Tooltip id={tooltipId} />}
+    </>
+  );
 }
