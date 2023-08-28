53e29c8a3161f9310a8d8ac2bc76837eb2dffd04
mathesond2 <mathesond2@gmail.com>
2023-08-28 12:51:25 -0400

    update Mint comp with useUpdate functionality

--- app/components/Dashboard/buttons/Mint.tsx
+++ app/components/Dashboard/buttons/Mint.tsx
@@ -1,15 +1,32 @@
 'use client';
 
-import { useAccount } from 'wagmi';
+import useHashesData from '@/app/hooks/useHashesData';
 import useMintNewHash from '@/app/hooks/useMintNewHash';
+import useUpdate from '@/app/hooks/useUpdate';
+import { useAccount } from 'wagmi';
 import Button from '../../common/Button';
 
 export default function Mint() {
   const { isConnected } = useAccount();
   const { handleMint } = useMintNewHash();
+  const { hashData, isError, isLoading: isLoadingHashesData } = useHashesData();
+  const { handleSave, isDisabled: isDisabledViaSave } = useUpdate();
+
   if (!isConnected) {
     return null;
   }
 
-  return <Button text={'UPDATE HASH'} buttonColor={'bg-primaryRed'} onClick={handleMint} />;
+  const button = hashData?.hashes.length
+    ? {
+        text: isLoadingHashesData ? '..LOADING' : 'UPDATE HASH',
+        fn: handleSave,
+        isDisabled: isDisabledViaSave || isLoadingHashesData,
+      }
+    : {
+        text: isLoadingHashesData ? '...LOADING' : 'MINT HASH',
+        fn: handleMint,
+        isDisabled: isLoadingHashesData,
+      };
+
+  return <Button text={button.text} buttonColor={'bg-primaryRed'} onClick={button.fn} disabled={button.isDisabled} />;
 }
