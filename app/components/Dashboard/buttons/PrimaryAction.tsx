cf8957328f5848ebdf8b7887be2e1a5fdb14c3be
mathesond2 <mathesond2@gmail.com>
2023-08-28 15:44:49 -0400

    add Update comp

--- app/components/Dashboard/buttons/PrimaryAction.tsx
+++ app/components/Dashboard/buttons/PrimaryAction.tsx
@@ -1,58 +1,9 @@
 'use client';
 
-import { useHashContext } from '@/app/contexts/HashContext';
 import useHashesData from '@/app/hooks/useHashesData';
-import useMintNewHash from '@/app/hooks/useMintNewHash';
-import useUpdate from '@/app/hooks/useUpdate';
-import { INITIAL_SELECTED_HASH, LOADING_TEXT, VERIFY_MESSAGE } from '@/app/util/constants';
-import { Tooltip } from 'react-tooltip';
-import { useAccount, useSignMessage } from 'wagmi';
-import Button from '../../common/Button';
-
-type Props = {
-  isLoadingHashesData: boolean;
-};
-
-function UpdateButton({ isLoadingHashesData }: Props) {
-  const { selectedHash } = useHashContext();
-  const { handleUpdate, isDisabled: isDisabledViaSave } = useUpdate();
-  const {
-    data: signedData,
-    isError: isSignedError,
-    isLoading: isSignedLoading,
-    isSuccess,
-    signMessage,
-  } = useSignMessage({
-    message: VERIFY_MESSAGE,
-  });
-
-  console.log('signedData', signedData);
-
-  const noHashSelected = selectedHash === INITIAL_SELECTED_HASH;
-  const isDisabled = isDisabledViaSave || isLoadingHashesData || noHashSelected || isSignedLoading;
-  const tooltipId = 'update-tooltip';
-
-  function handleClick() {
-    signMessage();
-    if (isSuccess && signedData) {
-      handleUpdate(signedData);
-    }
-  }
-
-  return (
-    <>
-      <Button
-        text={isLoadingHashesData ? LOADING_TEXT : 'UPDATE HASH'}
-        buttonColor={'bg-primaryRed'}
-        onClick={handleClick}
-        disabled={isDisabled}
-        data-tooltip-id={tooltipId}
-        data-tooltip-content={'Create a design and select a Hash to update.'}
-      />
-      {isDisabled && <Tooltip id={tooltipId} />}
-    </>
-  );
-}
+import { useAccount } from 'wagmi';
+import MintButton from './Mint';
+import { UpdateButton } from './Update';
 
 export default function PrimaryActionButton() {
   const { hashData, isError, isLoading } = useHashesData();
