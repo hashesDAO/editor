f14481fd9eabe80682819270911959979360caca
mathesond2 <mathesond2@gmail.com>
2023-08-29 13:17:02 -0400

    update image context if canvas isnt empty, pass image to endpoint

--- app/components/Editor/Editor.tsx
+++ app/components/Editor/Editor.tsx
@@ -6,6 +6,7 @@ import { useHashContext } from '../../contexts/HashContext';
 import { useTraitsContext } from '../../contexts/TraitsContext';
 import type { Trait } from '../../reducers/traitsReducer';
 import * as attributeLibrary from '../../util/attributeLibrary';
+import { useRenderedImageContext, useRenderedImageDispatch } from '@/app/contexts/RenderedImageContext';
 
 let p5: any;
 let p5Instance: any;
@@ -42,6 +43,8 @@ export default function Editor() {
   const p5Ref = useRef(document.createElement('div'));
   const { selectedHash } = useHashContext();
   const selectedTraits = useTraitsContext();
+  const updateRenderedImage = useRenderedImageDispatch();
+  const renderedImage = useRenderedImageContext();
 
   useEffect(() => {
     p5 = require('p5');
@@ -55,11 +58,20 @@ export default function Editor() {
       p5Instance.remove();
     }
     if (p5Ref.current.firstChild) {
-      const base64Image = canvasToBase64(p5Ref.current.firstChild as HTMLCanvasElement);
       p5Ref.current.removeChild(p5Ref.current.firstChild);
     }
     p5Instance = new p5(renderP5(selectedHash, selectedTraits), p5Ref.current);
   }, [selectedHash, selectedTraits]);
+
+  useEffect(() => {
+    if (p5Ref.current.firstChild) {
+      const base64Image = canvasToBase64(p5Ref.current.firstChild as HTMLCanvasElement);
+      if (base64Image === renderedImage) {
+        return;
+      }
+      updateRenderedImage(base64Image);
+    }
+  }, [renderedImage, p5Ref.current.firstChild]);
 
   return <div ref={p5Ref} className="h-screen" id="canvas-container"></div>;
 }
--- app/hooks/useUpdate.ts
+++ app/hooks/useUpdate.ts
@@ -1,30 +1,28 @@
 import { useHashContext } from '@/app/contexts/HashContext';
-import { useTraitsContext } from '@/app/contexts/TraitsContext';
 import { INITIAL_SELECTED_HASH, LOADING } from '@/app/util/constants';
 import { useReducer } from 'react';
 import { Address } from 'viem';
 import { useNetwork } from 'wagmi';
+import { useRenderedImageContext } from '../contexts/RenderedImageContext';
 import { fetchReducer, initialFetchReducerState } from '../reducers/fetchReducer';
 import { HashesData } from '../util/types';
 
 export default function useUpdate() {
   const [updateData, dispatchUpdateData] = useReducer(fetchReducer, initialFetchReducerState);
-  const selectedTraits = useTraitsContext();
   const { selectedHash } = useHashContext();
   const { chain } = useNetwork();
-  const isDisabled = updateData.loading || selectedTraits.length === 0 || selectedHash === INITIAL_SELECTED_HASH;
+  const image = useRenderedImageContext();
+  const isDisabled = updateData.loading || image.length === 0 || selectedHash === INITIAL_SELECTED_HASH;
 
   async function handleUpdate(signature: Address, address: Address, selectedHashData: HashesData) {
     dispatchUpdateData({ type: LOADING });
     await fetch('/api/update', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         hash: selectedHashData.hash_value,
         tokenId: selectedHashData.token_id,
-        image: selectedTraits.map((trait) => trait.id),
+        image,
         signature,
         address,
         chain: chain?.network,
