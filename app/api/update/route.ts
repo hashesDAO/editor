378fb8436f149b148db8cfcca421314c3eb26dd4
mathesond2 <mathesond2@gmail.com>
2023-08-30 11:07:43 -0400

    add PUT req to hashes site

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -1,4 +1,4 @@
-import { callReadOnlyFnFromHashesContract } from '@/app/util';
+import { callReadOnlyFnFromHashesContract, getChainIdFromNetworkName } from '@/app/util';
 import { messageToSign } from '@/app/util/constants';
 import { NextResponse } from 'next/server';
 import { Address, isAddressEqual, verifyMessage } from 'viem';
@@ -37,20 +37,18 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: 'address does not own token' }, { status: 400 });
   }
 
-  // const res = await fetch('https://data.mongodb-api.com/...', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'API-Key': process.env.DATA_API_KEY,
-  //   },
-  //   body: JSON.stringify({ time: new Date().toISOString() }),
-  // });
+  const chainId = getChainIdFromNetworkName(chain);
+  const res = await fetch(`https://staging.thehashes.xyz/api/token/${tokenId}?chain_id=${chainId}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ image }),
+  });
 
-  // const data = await res.json();
+  const data = await res.json();
 
-  // if (postError) {
-  //   return NextResponse.json({ error: `error posting data to DB: ${postError.message}` }, { status: 500 });
-  // }
+  if (res.status !== 200) {
+    return NextResponse.json({ error: data.error }, { status: res.status });
+  }
 
   return NextResponse.json({ data: 'hello' }, { status: 201 });
 }
